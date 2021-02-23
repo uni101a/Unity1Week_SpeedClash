3c0d5851fcc099273748426fab777f604a87a254
uni101 <Hanacchiglon07@gmail.com>
2021-02-23 22:18:11 +0900

    領域中にノーツが入ると破棄する処理を実装

--- DestroyZone.cs
+++ DestroyZone.cs
@@ -4,4 +4,21 @@ using UnityEngine;
 
 public class DestroyZone : MonoBehaviour
 {
+    private GameObject releaseObject = null;
+
+    public GameObject GetReleaseObject()
+    {
+        return releaseObject;
+    }
+
+    public void Reset()
+    {
+        releaseObject = null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        releaseObject = other.gameObject;
+    }
 }
