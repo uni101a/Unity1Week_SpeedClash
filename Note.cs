c2962d010cddf21d3541e9048fcffd9d767b495c
uni101 <Hanacchiglon07@gmail.com>
2021-02-23 22:17:01 +0900

    ノーツの移動、破棄を実装

--- Note.cs
+++ Note.cs
@@ -4,20 +4,89 @@ using UnityEngine;
 
 public class Note : MonoBehaviour
 {
+    private NotesManager _noteManager;
+    //このオブジェクトがステージ中何番目のノーツかを保持する
+    private int noteNumber = 0;
+    //1区切りに対してのノーツの座標
+    private Vector3 startPos = Vector3.zero;
+    private Vector3 endPos = Vector3.zero;
+
+    /// <summary>
+    /// ノーツオブジェクトの現在の状態
+    /// </summary>
+    private enum NOTE_STATE
+    {
+        IDLING, //待機中
+        MOVING, //移動中
+    }
+    private NOTE_STATE _noteState = NOTE_STATE.IDLING;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _noteManager = transform.parent.transform.parent.GetComponent<NotesManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // ! SUPRE IF STREAM !
+        if (!_noteManager.GetIsMoving())
+        {
+            if(_noteState == NOTE_STATE.MOVING)
+            {
+                _noteState = NOTE_STATE.IDLING;
+                noteNumber++;
+                SetPosition(transform.position, _noteManager.GetNotesPosition(noteNumber+1));
+            }
+
+            return;
+        }
+
+        if (_noteState == NOTE_STATE.IDLING)
+            _noteState = NOTE_STATE.MOVING;
+
+        Move(transform.position, endPos, Time.deltaTime * _noteManager.NoteSpeed);
     }
 
-    public void Move(Vector3 start, Vector3 end)
+    /// <summary>
+    /// 1区切りで移動するノーツの座標をセット
+    /// </summary>
+    /// <param name="start">スタート座標</param>
+    /// <param name="end">移動後の座標</param>
+    public void SetPosition(Vector3 start, Vector3 end)
     {
-        transform.position = Vector3.Lerp(start, end, 0.1f);
+        startPos = start;
+        endPos = end;
+    }
+
+    /// <summary>
+    /// ノーツを移動
+    /// </summary>
+    /// <param name="start">オブジェクトの現在座標</param>
+    /// <param name="end">目的の座標</param>
+    /// <param name="step">移動する値</param>
+    public void Move(Vector3 start, Vector3 end, float step)
+    {
+        transform.position = Vector3.MoveTowards(start, end, step);
+    }
+
+    /// <summary>
+    /// 変数を初期化
+    /// </summary>
+    public void DestroyProparties()
+    {
+        noteNumber = -1;
+        startPos = Vector3.zero;
+        endPos = Vector3.zero;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //ノーツがデストロイゾーンに入ったときオブジェクトを非アクティブ化
+        if(other.gameObject.tag == "DestroyZone")
+        {
+            DestroyProparties();
+            _noteManager.ReleaseNote(gameObject);
+        }
     }
 }
