f7928f02283f89ff190b262eacff67045572223f
uni101 <Hanacchiglon07@gmail.com>
2021-02-24 22:44:06 +0900

    全体の管理

--- GameManager.cs
+++ GameManager.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Text _scoreText;
+    [SerializeField] Text _comboText;
+
+    private Score _score;
+    private int totalScore = 0;
+    private int combo = 0;
+    private int MAX_DIGIT = 7;
+
+    private void Start()
     {
-        
+        _score = Score.GetInstance();
+        _comboText.text = "";
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateScore();
+        UpdateCombo();
+    }
+
+    public void UpdateScore()
+    {
+        int scoreVal = _score.TotalScore;
+        if (scoreVal == totalScore)
+            return;
+
+        totalScore = scoreVal;
+        _scoreText.text = totalScore.ToString().PadLeft(MAX_DIGIT, '0');
+    }
+
+    public void UpdateCombo()
+    {
+        int comboVal = _score.Combo;
+        if (comboVal == combo)
+            return;
+
+        combo = comboVal;
+        if (combo == 0)
+            _comboText.text = "";
+        else
+            _comboText.text = combo + " Combo!";
     }
 }
