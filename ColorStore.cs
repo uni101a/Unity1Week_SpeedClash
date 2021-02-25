2ccf0534f47ebeaf5eff1dbef31a80aba0bad25a
uni101 <Hanacchiglon07@gmail.com>
2021-02-26 03:19:21 +0900

    フィーバータイムの処理を追加

--- ColorStore.cs
+++ ColorStore.cs
@@ -1,15 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ColorStore : MonoBehaviour
 {
     private Material _material;
     private float storedLiquit = 0;
     private Color _color = new Color(0, 0, 0);
+    public Color GetMixiedColor()
+    {
+        return _color;
+    }
 
-    private int MaxMixingNum = 100;
-    private int mixedNum = 0;
+    private int maxMixingNum = 40;
+    public int MaxMixingNum
+    {
+        get { return maxMixingNum; }
+        set { maxMixingNum = value; }
+    }
+
+    private float FEAVERTIME = 5f;
+    public float GET_FEAVERTIME()
+    {
+        return FEAVERTIME;
+    }
+
+    private bool isFeaver = false;
+    public bool IsFeaver
+    {
+        get { return isFeaver; }
+    }
+
+    private Color feaverColor = new Color(1, 1, 1);
+    public Color FeaverColor
+    {
+        get { return feaverColor; }
+    }
 
     private void Start()
     {
@@ -18,6 +45,10 @@ public class ColorStore : MonoBehaviour
 
     private void Update()
     {
+        if (isFeaver)
+        {
+            DecreaseStore();
+        }
     }
 
     public void Store(Color clashColor)
@@ -26,21 +57,71 @@ public class ColorStore : MonoBehaviour
         MixColor(clashColor);
     }
 
+    public bool IsMaxStored()
+    {
+        return storedLiquit >= 1 ? true : false;
+    }
+
+    public bool IsEmpty()
+    {
+        return storedLiquit <= 0.0001 ? true : false;
+    }
+
     private void UpLiquid()
     {
         storedLiquit += 1.0f / MaxMixingNum;
-        if (storedLiquit >= 1.0f)
+        if (storedLiquit > 1.0f)
             return;
         _material.SetFloat("_Height", storedLiquit);
     }
 
     private void MixColor(Color clashColor)
     {
-        mixedNum++;
-
         Color mixedColor = (_color + clashColor) / 2;
         _color = mixedColor;
 
         _material.SetColor("_MixColor", _color);
+    }
+
+    private void DecreaseStore()
+    {
+        storedLiquit -= 1/ FEAVERTIME * Time.deltaTime;
+        _material.SetFloat("_Height", storedLiquit);
+
+        if (IsEmpty())
+        {
+            isFeaver = false;
+            storedLiquit = 0;
+            feaverColor = new Color(1, 1, 1);
+            _material.SetFloat("_Height", storedLiquit);
+            _material.SetColor("_MixColor", _color);
+        }
+    }
+
+    public void OnFeaver()
+    {
+        float[] colorVals = { _color.r, _color.g, _color.b };
+        Color fevC = new Color(1, 1, 1);
+        float tempMax = -1;
+        for(int i=0; i<colorVals.Length; i++)
+        {
+            if(tempMax < colorVals[i])
+            {
+                if (i == 0)
+                    fevC = new Color(1, 0, 0);
+                else if (i == 1)
+                    fevC = new Color(0, 1, 0);
+                else if(i == 2)
+                    fevC = new Color(0, 0, 1);
+                else
+                    fevC = new Color(1, 1, 1);
+
+                tempMax = colorVals[i];
+            }
+        }
+        feaverColor = fevC;
+        
+        _color = new Color(0, 0, 0);
+        isFeaver = true;
     }
 }
